c68e1d6c9356f9b6246e68ec53b1746d4dd46e98
weissercn <weissercn@gmail.com>
2016-08-31 14:21:08 -0400

    added systematics

--- adaptive_binning_chisquared_2sam/__init__.py
+++ adaptive_binning_chisquared_2sam/__init__.py
@@ -2,4 +2,4 @@ import sys
 import os
 os.environ["adaptive_binning_chisquared_2sam"]=os.getcwd()
 #sys.path.insert(0, './GoF')
-import chi2_regular_binning, chi2_adaptive_binning, chi2_plots, calling_chi2_example
+import chi2_regular_binning, chi2_adaptive_binning, chi2_adaptive_binning_systematics , chi2_plots, calling_chi2_example
